3ea41271acb92469e9715cdb0aff088f3b483770
Darktotaled <e.fialhosantos@yahoo.com.br>
2021-07-25 13:44:11 -0300

    estrutura cliente e produto

--- src/pages/Clientes.js
+++ src/pages/Clientes.js
@@ -1,6 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+function AssignValues() {
+  localStorage.setItem("nome", document.form1.nome.value);
+  localStorage.setItem("endereco", document.form1.endereco.value);
+  localStorage.setItem("telefone", document.form1.telefone.value);
+  localStorage.setItem("email", document.form1.email.value);
+}
+
 export default function Clientes() {
   return (
-    <h1> Clientes </h1>
+    <div>
+
+      <h1> Cadastro de clientes </h1>
+      <form name='form1' action='/'>
+        <div className='formulario'>
+          <label htmlFor='name'>Nome</label>
+          <input type="text" name='nome' />
+          <label htmlFor='endereco'>Endereço</label>
+          <input type="text" name='endereco' />
+          <label htmlFor='telefone'>Telefone</label>
+          <input type="text" name='telefone' />
+          <label htmlFor='email'>E-mail</label>
+          <input type="email" name='email' />
+        </div>
+      </form>
+      <div>
+        <input type="button" value="Cadastrar" onClick={AssignValues} />
+      </div>
+      <div>
+        <Link to="/"><button>Voltar</button></Link>
+      </div>
+    </div>
+
   )
 }
--- src/pages/Produtos.js
+++ src/pages/Produtos.js
@@ -1,6 +1,38 @@
 import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+function AssignValues() {
+  localStorage.setItem("id", document.form1.id.value);
+  localStorage.setItem("name", document.form1.name.value);
+  localStorage.setItem("price", document.form1.price.value);
+  localStorage.setItem("description", document.form1.description.value);
+}
+
 export default function Produtos() {
   return (
-    <h1> Produtos </h1>
+    <div>
+
+      <h1> Cadastro de produtos </h1>
+      <form name='form1' action='/'>
+        <div className='formulario'>
+          <label htmlFor='id'>Id</label>
+          <input type="text" name='id' />
+          <label htmlFor='name'>Nome</label>
+          <input type="text" name='name' />
+          <label htmlFor='price'>Preço</label>
+          <input type="text" name='price' />
+          <label htmlFor='description'>Descrição</label>
+          <input type="email" name='description' />
+        </div>
+      </form>
+      <div>
+        <input type="button" value="Cadastrar" onClick={AssignValues} />
+      </div>
+      <div>
+        <Link to="/"><button>Voltar</button></Link>
+      </div>
+    </div>
+
   )
 }
